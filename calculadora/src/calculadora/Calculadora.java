62a27e3b40dcccbd2e4ce4ea360e45055db84386
estudiantes <estudiantes@labing00>
2017-03-24 16:35:07 -0500

    creación de clase calculadora

---
+++ calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author estudiantes
+ */
+public class Calculadora {
+
+    double n1;
+    double n2;
+    
+    public double Sumar(){
+        
+        return(n1+n2);
+        
+    }
+    
+    public double Multiplicar(){
+        
+        return(n1*n2);
+        
+    }
+
+    public double Dividir(){
+        
+        return(n1/n2);
+        
+    }
+    
+    public double Restar(){
+        
+        return(n1-n2);
+        
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
